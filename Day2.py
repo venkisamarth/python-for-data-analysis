2214b8af627f4b0de4b398bbfc1ceb049c759542
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-06-13 23:56:29 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,33 @@
+# what is python 
+# python is the high level progamming language like like c and c++ 
+# types of progaming language 
+# python is the simple ,high level,interpreted, general purpose ,dynamycally typed and object oriented  programming language created bu Guido Van Rossum in 1991 
+# python is the older then the java language 
+# 
+
+# simple is simple 
+#python is easy to use 
+#python program are simple to write and hence it is easy 
+#to learn 
+
+# Lots of built in funciton we can use easy then the other programs 
+
+# # hello world in java program in java 
+# class Helloworld { 
+#     static public void main (String args []){
+#         system.out,println ("Hello world")
+#     }
+# }
+
+# ## Hello world in c++ 
+# # include <iostream.h>
+# main () {  
+#     cout<<"Hello world "<<endl;
+#     return 0;
+
+# }
+
+# Hello world progam in pyhton 
+print("Hello world this is the my first progam in the python ")
+
+
