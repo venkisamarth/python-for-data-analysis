f9efdf8f37afc70d679d2da759182321d0490582
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-12-29 22:32:41 +0530

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# varaibles and Operators i%%pythonon
+# python variable is containers that sotre values, 
+# pytho is not statically typed we do not need to declare
+# varaible is created befoe using them or declare their type a 
+# varaible is created the moment we first sasssig
+# a variable name must start with a letter or the undescore 
+# a varaible name cam only contain alpa numeric
+# variabel name can only contains alpha-numeric
+a=12
+print(a)
+# a varaible is created the moment you first assign a value to it
+x,y,z="orange","banana","cherry"
+print(a,y,z)
+
+
+# In[3]:
+
+
+#day 2 
+# varaible and Operators
+a=1
+print(a)
+
+
+# In[4]:
+
+
+b=2
+c=2.3
+print(type(a))
+print(type(c))
+print(a,b)
+
+
+# In[5]:
+
+
+x,y=1,23
+print('x',x)
+
+
+# In[6]:
+
+
+x="apple"
+print(x)
+
+
+# In[7]:
+
+
+student =23
+print(student)
+
+
+# In[10]:
+
+
+# 1@_=2
+# print(1@_)
+# this will give the error in 
+stu_1="venkatesh"
+print(stu_1)
+
+
+# In[12]:
+
+
+_a=5
+print(_a)
+
+
+# In[15]:
+
+
+# many value to multiple variabls
+x=y=z=2
+print(x)
+print(y)
+print(z)
+
+
+# In[16]:
+
+
+x,y,z="orange","banna","cherry"
+print(x,y,z)
+
+
+# In[18]:
+
+
+x=y=z="orange"
+print(x,y,z)
+
+
+# In[19]:
+
+
+# python out put variable 
+x="python"
+y="is"
+z="amzing"
+print(x,y,z)
+print(x)
+print(y)
+print(z)
+
+
+# In[20]:
+
+
+# # data types in python 
+# text type=str
+# numeric type =int float complx
+# sequnce type =list tuple 
+# mapping type 
+# boolean type
+# binary type
+x=4
+print(x)
+print(type(x))
+y=2.3
+print(type(y))
+y="This is the normal floating"
+print(type(y))
+
+
+# In[22]:
+
+
+z=2+5j
+print(z)
+print(type(z))
+
+
+# In[23]:
+
+
+a="this is a day 2 of the python"
+print(a)
+print(type(a))
+
+
+# In[24]:
+
+
+# sequance type of data in the python 
+b=[1,2,3,4,5,6,6,7]
+print(type(b))
+
+
+# In[25]:
+
+
+b={1,2,3,4,5,6,6}
+print(type(b))
+
+
+# In[26]:
+
+
+d=(1,2,2,3,4,4)
+print(type(d))
+
+
+# In[28]:
+
+
+get_ipython().run_line_magic('whos', '')
+
+
+# In[29]:
+
+
+# operators in python
+# Arithmetic Operators 
+a=5
+b=3
+print(a+b)
+
+
+# In[30]:
+
+
+a=5
+b=2.4
+print(a+b)
+
+
+# In[31]:
+
+
+a=5+3j
+b=4
+print(a+b)
+
+
+# In[33]:
+
+
+a="hello"
+b="students"
+print(a+" "+b)
+
+
+# In[34]:
+
+
+# substraction in python
+a=15 
+b=6 
+print(a-b)
+b=234.4
+p=4
+print(b-p)
+
+
+# In[35]:
+
+
+a="hellow"
+b="student"
+print(a-b)
+
+
+# In[36]:
+
+
+# multiple Operators in python 
+a=15
+b=56 
+print(a*b)
+print(a/b)
+
+
+# In[38]:
+
+
+# modulus Operators 
+a=19 
+b=3 
+print(a%b)
+
+
+# In[39]:
+
+
+# exponant
+a=423454
+print(a**55)
+
+
+# In[40]:
+
+
+a=((232*343)+(78.343/89)+(433*343))
+print(a)
+
+
+# In[41]:
+
+
+a=True
+b=False
+print(not(a))
+
+
+# In[ ]:
+
+
+
+
---
+++ day_one.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Hello world")
+
+
+# In[4]:
+
+
+print("Hello world this is the ammzing python clsss")
+
+
+# In[ ]:
+
+
+print('this is also valid int python program')
+
+
+# In[6]:
+
+
+print(2+6)
+
+
+# In[5]:
+
+
+# we are going to write our first program
+print("Hello world")
+
+
+# In[8]:
+
+
+# # how to do the comment in th%%pythonon
+# """ this is also one the way
+# to make comment in the python"""// this is the multiline comment in Python
+# this is over first dasy of the class 
+print(3+9)
+
+
+# In[ ]:
+
+
+
+
