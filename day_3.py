a051395c71471749f0bc8d08457c093417d7cbf5
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-12-29 23:55:15 +0530

    Add files via upload

---
+++ day_3.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+# # built in functions in python 
+# types of function that python suports 
+# Built in function 
+# python Recursive 
+# pythonon lambda function 
+# user defind function
+# abs ()
+x=abs(-234)
+print(abs(-23))
+print(x)
+
+
+# In[8]:
+
+
+y=abs(-3)
+print(y)
+
+
+# In[9]:
+
+
+z=abs(3+5j)
+print(abs(z))
+
+
+# In[10]:
+
+
+# Bin function in python
+print(bin(23324))
+
+
+# In[11]:
+
+
+print(bin(23))
+
+
+# In[12]:
+
+
+y=bin(23)
+print(y)
+
+
+# In[13]:
+
+
+# python bytes function 
+print(bytes(23))
+
+
+# In[14]:
+
+
+x=bytes(4)
+print(x)
+
+
+# In[16]:
+
+
+# # character function 
+# unicode
+x=chr(97)
+print(x)
+
+
+# In[17]:
+
+
+y=chr(98)
+print(y)
+
+
+# In[18]:
+
+
+z=chr(99)
+print(z)
+
+
+# In[19]:
+
+
+a=chr(99)
+print(a)
+
+
+# In[20]:
+
+
+# complex number in python 
+x=complex(4,4)
+print(x)
+
+
+# In[21]:
+
+
+y=complex(5,4)
+print(y)
+
+
+# In[22]:
+
+
+z=complex(4,3.3)
+print(z)
+
+
+# In[23]:
+
+
+z=complex("2")
+print(z)
+
+
+# In[24]:
+
+
+# float function in python
+x=float(3)
+print(x)
+
+
+# In[25]:
+
+
+print(float(4.454))
+
+
+# In[27]:
+
+
+# in function in python 
+# int function in python 
+x=int(3223)
+print(x)
+
+
+# In[28]:
+
+
+print(int(12.34))
+
+
+# In[30]:
+
+
+# complex number is able to convert to int 
+z=complex(3,5)
+print(int(z))
+
+
+# In[31]:
+
+
+# str() function in python 
+a=str("23343")
+print(type(a))
+
+
+# In[32]:
+
+
+
+print(str(243))
+
+
+# In[33]:
+
+
+print(print(str(343)))
+
+
+# In[34]:
+
+
+help(str)
+
+
+# In[38]:
+
+
+# help() function use in python 
+help(print)
+
+
+# In[39]:
+
+
+print(help(float))
+
+
+# In[41]:
+
+
+# input() function in python 
+x=input("enter your name ")
+print( "hello",x)
+
+
+# In[42]:
+
+
+x=input("enter your age ")
+print(x)
+
+
+# In[43]:
+
+
+first_name=input("enter your name")
+second_name=input("enter your second name")
+print(first_name,second_name)
+
+
+# In[ ]:
+
+
+
+
