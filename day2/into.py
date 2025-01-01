f017608db8f354be50624f33728a92642ad54b53
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2025-01-01 23:44:01 +0530

    Add files via upload

---
+++ day2/into.py
@@ -0,0 +1,52 @@
+import sys 
+# sys.path.append(" enter here the your directory ")
+# from my_modul import find_index
+# # from my_modul import find_index,test
+# # import my_modul as mm
+# from my_modul import *
+import random
+courses=["Histry","Math","Physics","ComSci"]
+random_course=random.choice(courses)
+print(random_course)
+# index=find_index(courses,"Physics")
+# print(index)
+
+# import sys
+# print(sys.path)
+
+
+import math
+rads=math.radians(34)
+print(rads)
+
+sinevalue=math.sin(23)
+print(sinevalue)
+import datetime
+import calendar 
+today=datetime.date.today()
+print(today)
+
+print (calendar.isleap(2025))
+
+import datetime
+import calendar 
+today =datetime.date.today()
+print (today)
+
+
+import os 
+print(os.getcwd())
+print(os.__file__)
+
+import antigravity
+
+
+
+
+
+
+
+
+
+
+
---
+++ day2/my_modul.py
@@ -0,0 +1,8 @@
+print("Imported my_module....")
+test ="Test String"
+
+def find_index(to_search,target): 
+    """ Find the index of value in a sequnce"""
+    for i,value in enumerate(to_search): 
+        return i
+    return -1 
